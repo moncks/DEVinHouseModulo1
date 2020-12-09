03f8993aaf0fec5132f30c186514b7ae8bb8ad03
Brayan Moncks <brayanbmn@hotmail.com>
2020-12-09 18:17:24 -0300

    Adicionados exercicios da semana 4

---
+++ Semana4/LivrosMaisVendidos/script.js
@@ -0,0 +1,10 @@
+// Crie uma função que apresente o ranking dos livros mais vendidos. 
+// Você precisa diferenciar a posição apenas dos 3 primeiros do ranking, 
+// os demais serão apenas listados.
+
+function imprimirMaisVendidos(maisVendido, segundo, terceiro, ...livros) {
+    console.log("Mais Vrndido: ", maisVendido)
+    console.log("Segundo: ", segundo)
+    console.log("Terceiro: ", terceiro)
+    console.log("Sequência da lista: ", livros)
+}
