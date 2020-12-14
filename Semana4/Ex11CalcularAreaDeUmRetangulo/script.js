4d8dc8b454761d4c2a4eaadc73468badf31a854f
Brayan Moncks <brayanbmn@hotmail.com>
2020-12-14 18:45:06 -0300

    Adicionado exercicio: 11 concluido

---
+++ Semana4/Ex11CalcularAreaDeUmRetangulo/script.js
@@ -0,0 +1,7 @@
+/*Crie uma arrowFunction que receba um objeto contendo altura e 
+largura de um retângulo e retorne a área do retângulo. */
+
+const areaRetangulo = (b, h) => b * h;
+
+const area = areaRetangulo(50, 90);
+console.log(`A área do retângulo é de ${area}cm²`)
